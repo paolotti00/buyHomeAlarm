193ebc373a197b11575275461d337ef015391024
paolotti00 <paolo.tripodi@aubay.it>
2022-04-03 22:29:35 +0200

    telegram devo prod config

--- classes.py
+++ classes.py
@@ -61,6 +61,18 @@ class Search:
 
 
 # configuration file
+class Bot:
+    def __init__(self, d=None):
+        common_init(self, d)
+
+    api_token = None
+
+
+class Telegram:
+    def __init__(self, d=None):
+        common_init(self, d)
+
+    bot: Bot()
 
 
 class Sender:
@@ -105,6 +117,7 @@ class Config:
     db: DB = DB()
     sites: [Site] = [Site()]
     supported_sites_conf: [Site] = [Site()]
+    telegram: Telegram = Telegram()
     searches: [Search] = [Search()]
 
 
